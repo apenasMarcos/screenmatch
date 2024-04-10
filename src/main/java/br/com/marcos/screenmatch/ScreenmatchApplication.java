41f5b5ebb6956dd824d77aa9afc733a0fef0f1e8
Marcos Paulo <marcos.paulo.mpsb99@gmail.com>
2024-04-10 16:24:17 -0300

    feature: Desserializando com jackson

--- src/main/java/br/com/marcos/screenmatch/ScreenmatchApplication.java
+++ src/main/java/br/com/marcos/screenmatch/ScreenmatchApplication.java
@@ -16,7 +16,6 @@ public class ScreenmatchApplication implements CommandLineRunner {
 	public void run(String... args) throws Exception {
 		var consumoApi = new ConsumoApi();
 		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=6585022c");
-		json = consumoApi.obterDados("https://coffee.alexflipnote.dev/random.json");
 		System.out.println(json);
 	}
 }
