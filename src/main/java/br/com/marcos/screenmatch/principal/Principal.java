55161c17c5796a5998d4b8eb24c4f393255e690d
Marcos Paulo <marcos.paulo.mpsb99@gmail.com>
2024-05-19 06:44:51 -0300

    feature: adjusting somenthings

--- src/main/java/br/com/marcos/screenmatch/principal/Principal.java
+++ src/main/java/br/com/marcos/screenmatch/principal/Principal.java
@@ -19,27 +19,36 @@ public class Principal {
 
     private final ConvertData convertData = new ConvertData();
 
-    private final String API_KEY = "";
+    private final String API_KEY = "6585022c";
 
     private final String URL = "https://www.omdbapi.com/?apikey=" + API_KEY + "&t=";
 
 
     public void displayMenu(){
-        System.out.println("Enter the name of the series to search");
+        try {
+            System.out.println("Enter the name of the series to search");
 
-        String seriesName = inputScanner.nextLine().replace(" ", "+");
-        SerieData seriesData = fetchData(apiConsumer, convertData, URL + seriesName, SerieData.class);
+            String seriesName = inputScanner.nextLine().replace(" ", "+");
+            SerieData seriesData = fetchData(apiConsumer, convertData, URL + seriesName, SerieData.class);
 
-        System.out.println(seriesData);
+            System.out.println(seriesData);
 
-        List<SeasonData> seasonDataList = new ArrayList<>();
-        IntStream.range(1, seriesData.totalSeasons() + 1)
-                .boxed()
-                .forEach(i -> {
-                    SeasonData seasonData = fetchData(apiConsumer, convertData, URL + seriesName + "&season=" + i, SeasonData.class);
-                    seasonDataList.add(seasonData);
-                    System.out.println(seasonData);
-                });
+            List<SeasonData> seasonDataList = new ArrayList<>();
+            IntStream.range(1, seriesData.totalSeasons() + 1)
+                    .boxed()
+                    .forEach(i -> {
+                        SeasonData seasonData = fetchData(apiConsumer, convertData, URL + seriesName + "&season=" + i, SeasonData.class);
+                        seasonDataList.add(seasonData);
+                    });
+
+            seasonDataList.stream()
+                    .flatMap(seasonData -> seasonData.episodes().stream())
+                    .map(EpisodeData::title)
+                    .forEach(System.out::println);
+
+        } catch (Exception e) {
+            System.err.println("An error occurred while processing the request: " + e.getMessage());
+        }
     }
 
     private static <T> T fetchData(ApiConsumer apiConsumer, ConvertData convertData, String url, Class<T> clazz) {
